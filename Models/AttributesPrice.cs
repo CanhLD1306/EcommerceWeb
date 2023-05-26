6722cf487c03fd3f079cf0e2debf6a3bc118669f
CanhLD1306 <CanhLD1306@gmail.com>
2023-05-26 08:03:50 +0700

    Manage access rights

---
+++ Models/AttributesPrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EcommerceWeb.Models
+{
+    public partial class AttributesPrice
+    {
+        public int AttributesPriceId { get; set; }
+        public int? AtrributesId { get; set; }
+        public int? ProductId { get; set; }
+        public int? Price { get; set; }
+        public int Actice { get; set; }
+
+        public virtual Attribute Atrributes { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EcommerceWeb.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,9 @@ namespace EcommerceWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var stringConnectDb = Configuration.GetConnectionString("dbEcommerceWeb");
+            services.AddDbContext<dbEcommerceContext>(options => options.UseSqlServer(stringConnectDb));
+
             services.AddControllersWithViews();
         }
 
